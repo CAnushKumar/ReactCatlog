552e745ffd5b1a3ba20163f86f288292c926bd28
CAnushKumar <83301588+CAnushKumar@users.noreply.github.com>
2023-07-26 20:30:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ProductList from "./ProductList";
+import Navbar from "./Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <ProductList></ProductList>
+    </div>
+  );
+};
+
+export default App;
